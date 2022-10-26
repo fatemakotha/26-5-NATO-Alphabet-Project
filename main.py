bcecaa53b0b1bc106715c34be8c3c930ee15f75b
fatemakotha <fatema.alam.kotha@gmail.com>
2022-10-26 17:42:45 +0600

    Added exceptions

--- main.py
+++ main.py
@@ -12,14 +12,34 @@ print(data)
 # 3       D     Delta........... and so on. THIS IS A DATA FRAME
 
 
-#TODO 1. Create a dictionary in this format: #To iterate through a DataFrame we use dataframe.iterrows() **
+#Create a dictionary in this format: #To iterate through a DataFrame we use dataframe.iterrows() **
 #dictionary comprehension used
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()} #letter and code are 2 headings in csv file.
 # row is a fixed name which we tap into, to access anything rom the 2 headings
 print(phonetic_dict)
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+#Create a list of the phonetic code words from a word that the user inputs.
+# is_on = True
+# while is_on:
+#     try:
+#         word = input("Enter a word: ").upper()
+#         new_list = [phonetic_dict[letter] for letter in word] #list comprehension used
+#     except KeyError: #catches the EXCEPTION when user enters a number instead of a letter
+#         print("Only letters please")
+#     else:
+#         print(new_list)
 
-word = input("Enter a word: ").upper()
-new_list = [phonetic_dict[letter] for letter in word] #list comprehension used
-print(new_list)
+#__________________________OR___________________________________________
+
+#Create a list of the phonetic code words from a word that the user inputs.
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        new_list = [phonetic_dict[letter] for letter in word] #list comprehension used
+    except KeyError: #catches the EXCEPTION when user enters a number instead of a letter
+        print("Only letters please")
+        word = input("Enter a word: ").upper()
+    else:
+        print(new_list)
+
+generate_phonetic()
